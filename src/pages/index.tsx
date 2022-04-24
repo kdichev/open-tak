01900787df81521445e757f5be944dde2c2a5e80
kdichev <konstantin.dichev@gmail.com>
2022-04-24 22:15:51 +0200

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,109 @@
 import React from "react";
 import { Map, Marker } from "pigeon-maps";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+  useMutation,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://opentak.herokuapp.com/v1/graphql",
+  cache: new InMemoryCache(),
+});
 
 const IndexPage = () => {
-  const [coords, setcoords] = React.useState({});
+  const [userId, setUserId] = React.useState(null);
+  const { data, loading, error } = useQuery(
+    gql`
+      query MyQuery($user_id: uuid!) {
+        user_location(
+          order_by: { created_at: desc }
+          limit: 1
+          where: { user_id: { _eq: $user_id } }
+        ) {
+          longitude
+          latitude
+          created_at
+        }
+      }
+    `,
+    { variables: { user_id: userId } }
+  );
+  const [addNewUserLocation] = useMutation(gql`
+    mutation MyMutation(
+      $latitude: float8!
+      $longitude: float8!
+      $user_id: uuid!
+    ) {
+      insert_user_location(
+        objects: {
+          user_id: $user_id
+          longitude: $longitude
+          latitude: $latitude
+        }
+      ) {
+        returning {
+          created_at
+        }
+      }
+    }
+  `);
+
   React.useEffect(() => {
     navigator.geolocation.watchPosition((position) => {
-      console.log(position.coords.latitude);
-      console.log(position.coords.longitude);
-      setcoords({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      });
+      if (userId) {
+        addNewUserLocation({
+          variables: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            user_id: userId,
+          },
+        });
+      }
     });
   }, []);
 
   return (
     <div>
-      <p>{coords.latitude}</p>
-      <p>{coords.longitude}</p>
-      {coords.latitude && coords.longitude && (
+      <p>{data?.user_location[0].latitude}</p>
+      <p>{data?.user_location[0].longitude}</p>
+      <input
+        type="text"
+        onChange={(e) => {
+          setUserId(e.target.value);
+        }}
+      />
+      <br />
+      <br />
+      {data?.user_location[0].latitude && data?.user_location[0].longitude && (
         <Map
           height={300}
-          defaultCenter={[coords.latitude, coords.longitude]}
-          defaultZoom={11}
+          defaultCenter={[
+            data?.user_location[0].latitude,
+            data?.user_location[0].longitude,
+          ]}
+          defaultZoom={15}
         >
-          <Marker width={50} anchor={[coords.latitude, coords.longitude]} />
+          <Marker
+            width={50}
+            anchor={[
+              data?.user_location[0].latitude,
+              data?.user_location[0].longitude,
+            ]}
+          />
         </Map>
       )}
     </div>
   );
 };
 
-export default IndexPage;
+const BasePage = (props) => (
+  <ApolloProvider client={client}>
+    <IndexPage {...props} />
+  </ApolloProvider>
+);
+
+export default BasePage;
